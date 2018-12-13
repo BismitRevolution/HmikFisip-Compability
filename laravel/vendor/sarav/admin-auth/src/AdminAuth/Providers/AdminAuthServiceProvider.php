302d084fcd98764b0ef7de5d8a1cfc541520412d
cymon1997 <cybermonster82@yahoo.com>
2018-12-13 13:48:48 +0700

    deployment mode

---
+++ laravel/vendor/sarav/admin-auth/src/AdminAuth/Providers/AdminAuthServiceProvider.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Sarav\AdminAuth\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Sarav\AdminAuth\Console\AdminAuth;
+
+class AdminAuthServiceProvider extends ServiceProvider
+{
+	/**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerAuthConfig();
+        $this->registerAuthCommand();
+        $this->publishAdminConfig();
+    }
+
+    /**
+     * Registers the auth configuration
+     * dynamically in config/auth.php file.
+     */
+    public function registerAuthConfig()
+    {
+    	$config = $this->app['config']['auth'];
+
+        $config['guards']['admin'] = [
+            'driver' => 'session',
+            'provider' => 'admins',
+        ];
+
+        $config['providers']['admins'] = [
+            'driver' => 'eloquent',
+            'model' => \App\Admin::class,
+        ];
+
+        $config['passwords']['admins'] = [
+            'provider' => 'admins',
+            'email'    => 'admin.auth.emails.password',
+            'table'    => 'password_resets',
+            'expire'   => 60,
+        ];
+
+        $this->app['config']['auth'] = $config;
+    }
+
+    /**
+     * Registers the admin:auth command
+     */
+    public function registerAuthCommand()
+    {
+    	$this->app->singleton('command.admin.auth', function () {
+            return new AdminAuth();
+        });
+        
+        $this->commands(['command.admin.auth']);
+    }
+
+    public function publishAdminConfig()
+    {
+    	$source = realpath(__DIR__.'/../config/admin.php');
+        $this->publishes([$source => config_path('admin.php')]);
+    }
+}
